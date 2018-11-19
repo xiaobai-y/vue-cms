9765435390334442193f0333bd4169ecc368878e
yaya <18738611628@163.com>
2018-11-19 12:54:22 +0800

    vue-cms

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <h1>这是我的app组件</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+<h2>这是我的helloworld组件</h2>
+</template>
+
+<script>
+
+</script>
+
+<style >
+
+</style>
